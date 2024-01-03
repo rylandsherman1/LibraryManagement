8b90eb26e21021ef479a484b678cd8e218e4ad51
Kimberly Palmer <kimberlylpalmer@gmail.com>
2024-01-03 04:57:59 -0800

    added menu functionality

--- cli.py
+++ cli.py
@@ -32,9 +32,10 @@ def manage_books():
         print("2. Delete a new book")
         print("3. View all books")
         print("4. Find a book (by ISBN, title, author)")
-        print("5. Return to main menu")
+        print("5. View All Members who have borrowed a specific book")
+        print("6. Return to main menu")
         
-        choice = input("Enter your choice (1-5): ")
+        choice = input("Enter your choice (1-6): ")
         
         if choice == '1':
             add_book()
@@ -45,6 +46,8 @@ def manage_books():
         elif choice == '4':
             find_book()
         elif choice == '5':
+            view_all_members_who_borrowed_a_specific_book
+        elif choice == '6':
             break
         else: 
             print("Invalid choice, please choose a number between 1-5.")
@@ -61,6 +64,9 @@ def view_all_books():
 def find_book():
     #prompt user to enter search criteria (ISBN, title, author )
     print("Find Book functionality not yet implemented")
+def view_all_members_who_borrowed_a_specific_book():
+    #prompt user to enter search criteria (ISBN, title, author )
+    print("View All Members Who Borrowed A Specific Book functionality not yet implemented")
     
 def manage_members():
     while True:
@@ -69,12 +75,13 @@ def manage_members():
         print("2. Delete a member")
         print("3. View all members")
         print("4. Find a member (by membership number, name, email)")
-        print("5. Return to main menu")
+        print("5. Find All Books Borrowed by Member")
+        print("6. Return to main menu")
         
         choice = input("Enter your choice (1-5): ")
         
         if choice == '1':
-            add_member()
+            register_member()
         elif choice == '2':
             delete_member()
         elif choice == '3':
@@ -82,6 +89,8 @@ def manage_members():
         elif choice == '4':
             find_member()
         elif choice == '5':
+            find_all_books_borrowed_by_member()
+        elif choice == '6':
             break
         else: 
             print("Invalid choice, please choose a number between 1-5.")
@@ -98,6 +107,9 @@ def view_all_members():
 def find_member():
     #prompt user to enter search criteria (membership number, name, email )
     print("Find Member functionality not yet implemented")
+def find_all_books_borrowed_by_member():
+    #prompt user to enter search criteria (membership number, name, email )
+    print("Find All Books Borrowed By Member functionality not yet implemented")
         
 def manage_borrow_records():
     #Implementation for managing borrow_records
@@ -106,7 +118,7 @@ def manage_borrow_records():
         print("1. Create a new borrow record")
         print("2. Delete a borrow record")
         print("3. View all borrow record")
-        print("4. View books borrowe by a member")
+        print("4. View books borrowed by a member")
         print("5. View members who borrowed a book")
         print("6. Return to main menu")
         
