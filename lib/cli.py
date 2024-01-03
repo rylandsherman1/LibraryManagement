33056cf2c5a24e09be5f7e822ae6e47af710f087
Kimberly Palmer <kimberlylpalmer@gmail.com>
2024-01-03 09:28:59 -0800

    Latest push

--- lib/cli.py
+++ lib/cli.py
@@ -1,8 +1,13 @@
-from Book import create_book
+from Book import create_book, find_book_by_isbn, find_book_by_title, find_book_by_id
+from BorrowRecord import get_borrow_records_by_book_id, get_borrow_records_by_member_id
+from Members import create_member as create_member_db
+from Members import delete_member as delete_member_db
+from Members import get_all_members, find_member_by_id, find_member_by_name, find_member_by_email
 from sqlalchemy.orm import Session
 from database import SessionLocal
-from Book import find_book_by_isbn, find_book_by_title
-from BorrowRecord import get_borrow_records_by_book_id
+
+from Book import detel_book as delete_book_db
+from BorrowRecord import delete_borrow_record as delete_borrow_record_db
 
 
 def main_menu():
@@ -13,23 +18,24 @@ def main_menu():
         print("2. Manage Members")
         print("3. Manage Borrow Records")
         print("4. Exit")
-        
+
         choice = input("Enter your choice(1-4): ")
-        
-        if choice == '1':
+
+        if choice == "1":
             manage_books()
-        elif choice == '2':
+        elif choice == "2":
             manage_members()
-        elif choice == '3':
+        elif choice == "3":
             manage_borrow_records()
-        elif choice == '4':
+        elif choice == "4":
             print("Thank yoiu for using the Library Management System. Goodbye")
             break
         else:
             print("Invalid choice, please choose a number between 1-4.")
 
+
 def manage_books():
-    #Implementation for managing books
+    # Implementation for managing books
     while True:
         print("\nManage Books\n")
         print("1. Add a new book")
@@ -38,39 +44,40 @@ def manage_books():
         print("4. Find a book (by ISBN, title, author)")
         print("5. View All Members who have borrowed a specific book")
         print("6. Return to main menu")
-        
+
         choice = input("Enter your choice (1-6): ")
-        
-        if choice == '1':
+
+        if choice == "1":
             add_book()
-        elif choice == '2':
+        elif choice == "2":
             delete_book()
-        elif choice == '3':
+        elif choice == "3":
             view_all_books()
-        elif choice == '4':
+        elif choice == "4":
             find_book()
-        elif choice == '5':
+        elif choice == "5":
             view_all_members_who_borrowed_a_specific_book
-        elif choice == '6':
+        elif choice == "6":
             break
-        else: 
+        else:
             print("Invalid choice, please choose a number between 1-5.")
 
+
 def add_book():
-    #prompt for book details and add to the database
+    # prompt for book details and add to the database
     print("Adding a new book: ")
     title = input("Enter book title: ")
     author = input("Enter author name: ")
     isbn = input("Enter ISBN: ")
     available_copies = input("Enter number of available copies: ")
-    
+
     book_data = {
-        'title': title,
-        'author': author,
-        'isbn': isbn,
-        'available_copies': available_copies,
+        "title": title,
+        "author": author,
+        "isbn": isbn,
+        "available_copies": available_copies,
     }
-    
+
     db = SessionLocal()
     try:
         new_book = create_book(db, book_data)
@@ -79,13 +86,14 @@ def add_book():
         print(f"An error occurred: {e}")
     finally:
         db.close()
-        
+
+
 def delete_book():
-    #prompt for book identifiers and delete it from the database
+    # prompt for book identifiers and delete it from the database
     book_id = input("Enter the book ID to delete: ")
     db = SessionLocal()
     try:
-        if delete_book(db, int(book_id)):
+        if delete_book_db(db, int(book_id)):
             print("Book deleted from database.")
         else:
             print("Book not found.")
@@ -93,27 +101,28 @@ def delete_book():
         print("Invalid input. Please enter a valid book ID.")
     finally:
         db.close()
-    
-    
+
+
 def view_all_books():
-    #display all books from the database
+    # display all books from the database
     db = SessionLocal()
     try:
         books = get_all_books(db)
         for book in books:
             print(f"{book.id}: {book.title} by {book.author}")
     finally:
-        db.close()    
-    
+        db.close()
+
+
 def find_book():
-    #prompt user to enter search criteria (ISBN, title, author )
+    # prompt user to enter search criteria (ISBN, title, author )
     search_option = input("Search by (1) ID or (2) ISBN: ")
     db = SessionLocal()
     try:
-        if search_option == '1':
+        if search_option == "1":
             book_id = input("Enter book ID: ")
             book = find_book_by_id(db, int(book_id))
-        elif search_option == '2':
+        elif search_option == "2":
             isbn = input("Enter ISBN: ")
             book = find_book_by_(db, isbn)
         else:
@@ -128,32 +137,34 @@ def find_book():
         print("invalid input.")
     finally:
         db.close()
-    
-    
+
+
 def view_all_members_who_borrowed_a_specific_book():
-    #prompt user to enter search criteria (ISBN, title, author )
+    # prompt user to enter search criteria (ISBN, title, author )
     print("\nView All Members Who Borrowed A Specific Book")
-    
+
     search_option = input("search by (1) ISBN or (2) Title: ")
     db = SessionLocal()
-    
+
     try:
-        if search_option == '1':
+        if search_option == "1":
             isbn = input("Enter ISBN: ")
             book = find_book_by_isbn(db, isbn)
-        elif search_option == '2':
+        elif search_option == "2":
             title = input("Enter book title: ")
             book = find_book_by_title(db, title)
         else:
             print("Invalid Option.")
             return
-        
+
         if book:
             borrow_records = get_borrow_records_by_book_id(db, book.id)
             if borrow_records:
                 print(f"\nMembers who borrowed '{book.title}':")
                 for record in borrow_records:
-                    print(f"Member ID: {record.member.id} | Borrow Date: {record.borrow_date}")
+                    print(
+                        f"Member ID: {record.member.id} | Borrow Date: {record.borrow_date}"
+                    )
             else:
                 print("No borrow recrods found for this book.")
         else:
@@ -162,8 +173,8 @@ def view_all_members_who_borrowed_a_specific_book():
         print(f"An error occurred: {e}")
     finally:
         db.close
-                
-    
+
+
 def manage_members():
     while True:
         print("\nManage Members\n")
@@ -173,42 +184,128 @@ def manage_members():
         print("4. Find a member (by membership number, name, email)")
         print("5. Find All Books Borrowed by Member")
         print("6. Return to main menu")
-        
+
         choice = input("Enter your choice (1-5): ")
-        
-        if choice == '1':
+
+        if choice == "1":
             register_member()
-        elif choice == '2':
+        elif choice == "2":
             delete_member()
-        elif choice == '3':
+        elif choice == "3":
             view_all_members()
-        elif choice == '4':
+        elif choice == "4":
             find_member()
-        elif choice == '5':
+        elif choice == "5":
             find_all_books_borrowed_by_member()
-        elif choice == '6':
+        elif choice == "6":
             break
-        else: 
+        else:
             print("Invalid choice, please choose a number between 1-5.")
 
+
 def register_member():
-    #prompt for member details and add to the database
-    print("Register Member functionality not yet implemented")
+    # prompt for member details and add to the database
+    print("Register New Member: ")
+    name = input("Enter Member's Name: ")
+    email = input("Enter Member's Email: ")
+    membership_number = input(
+        "Enter membership number: "
+    )  # should this be automatically generated?
+
+    member_data = {
+        "name": name,
+        "email": email,
+        "membership_number": membership_number,
+    }
+
+    db = SessionLocal()
+    try:
+        new_member = create_member_db(db, member_data)
+        print(f"Member added: {new_member.name} with email {new_member.email}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        db.close()
+
+
 def delete_member():
-    #prompt for member identifiers and delete them from the database
-    print("Delete Member functionality not yet implemented")
+    # prompt for member identifiers and delete them from the database
+    member_id = input("Enter the member ID to delete: ")
+    db = SessionLocal()
+    try:
+        if delete_member_db(db, int(member_id)):
+            print("Member deleted from database.")
+        else:
+            print("Member not found.")
+
+    except ValueError:
+        print("Invalid input. Please enter a valid member ID.")
+    finally:
+        db.close()
+
+
 def view_all_members():
-    #display all members from the database
-    print("View All Members functionality not yet implemented")
+    # display all members from the database
+    db = SessionLocal()
+    try:
+        members = get_all_members(db)
+        for member in members:
+            print(f"{member.id}: {member.name} - {member.email}")
+    finally:
+        db.close()
+
+
 def find_member():
-    #prompt user to enter search criteria (membership number, name, email )
-    print("Find Member functionality not yet implemented")
+    # prompt user to enter search criteria (membership number, name, email )
+    search_option = input("Seach by (1) ID, (2) Name, or (3) Email: ")
+    db = SessionLocal()
+    try:
+        if search_option == "1":
+            member_id = input("Enter memver ID: ")
+            member = find_member_by_id(db, int(member_id))
+        elif search_option == "2":
+            name = input("Enter member name: ")
+            member = find_member_by_name(db, name)
+        elif search_option == "3":
+            email = input("Enter member email: ")
+            member = find_member_by_email(db, email)
+        else:
+            print("Invalid option.")
+            return
+
+        if member:
+            print(f"Found Member: {member.name} with email {member.email}")
+        else:
+            print("No member found with the given criteria.")
+    except ValueError:
+        print("Invalid input.")
+    finally:
+        db.close()
+
+
 def find_all_books_borrowed_by_member():
-    #prompt user to enter search criteria (membership number, name, email )
-    print("Find All Books Borrowed By Member functionality not yet implemented")
-        
+    # prompt user to enter search criteria (membership number, name, email )
+    member_id = input("Enter the member ID: ")
+    db = SessionLocal()
+    try:
+        borrow_records = get_borrow_records_by_member_id(db, int(member.id))
+        if borrow_records:
+            print(f"Books borrowed by Member ID {member_id}: ")
+            for record in borrow_records:
+                book = find_book_by_id(db, record.book_id)
+                print(
+                    f"Book ID: {book.id}, Title: {book.title}, Borrow Date: {record.borrow_date}"
+                )
+        else:
+            print("No borrow records found for this member.")
+    except ValueError:
+        print("Invalid input. Please enter a valid member ID.")
+    finally:
+        db.close()
+
+
 def manage_borrow_records():
-    #Implementation for managing borrow_records
+    # Implementation for managing borrow_records
     while True:
         print("\nManage Borrow Records\n")
         print("1. Create a new borrow record")
@@ -217,36 +314,44 @@ def manage_borrow_records():
         print("4. View books borrowed by a member")
         print("5. View members who borrowed a book")
         print("6. Return to main menu")
-        
+
         choice = input("Enter your choice (1-6): ")
-        
-        if choice == '1':
+
+        if choice == "1":
             create_borrow_record()
-        elif choice == '2':
+        elif choice == "2":
             delete_borrow_record()
-        elif choice == '3':
+        elif choice == "3":
             view_all_borrow_records()
-        elif choice == '4':
+        elif choice == "4":
             view_books_borrowed_by_member()
-        elif choice == '5':
+        elif choice == "5":
             view_members_who_borrowed_book()
-        elif choice == '6':
+        elif choice == "6":
             break
         else:
             print("Invalic choice, please choose a number between 1-6.")
 
+
 def create_borrow_record():
     print("create Borrow Record functionality not yet implemented")
+
+
 def delete_borrow_record():
     print("delete Borrow Record functionality not yet implemented")
+
+
 def view_all_borrow_records():
     print("View All Borrow Records functionality not yet implemented")
+
+
 def view_books_borrowed_by_member():
     print("View Books Borrowed by a Member functionality not yet implemented")
+
+
 def view_members_who_borrowed_book():
     print("View Members Who Borrowed a Book functionality not yet implemented")
-    
+
+
 if __name__ == "__main__":
     main_menu()
-    
-    
